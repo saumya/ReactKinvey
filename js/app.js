76c6f38aca5981679c4840d2c186b5200c0df112
saumya <2saumyaray@gmail.com>
2016-01-30 10:14:56 +0530

    Code cleanup

--- js/app.js
+++ js/app.js
@@ -2,15 +2,11 @@
 (function(){
   var React = require('react');
   var ReactDOM = require('react-dom');
-  //var KinveyAPI = require('./utils/kinveyAPI');
-  //
+  // Controller View
   var Home = require('./components/appHome.react');
-  //
+  // Render
   var App = ReactDOM.render(
     <Home name="Forest" message="Happy to see you on React."/>,
     document.getElementById('react-app')
   );
-  //
-  //KinveyAPI.init();
-  //KinveyAPI.initServer();
 })();
