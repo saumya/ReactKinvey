13daab22953ef3708bea55e9720f4db5a8b3dda7
saumya <2saumyaray@gmail.com>
2016-01-30 10:15:23 +0530

    Code Cleaup

--- js/app.js
+++ js/app.js
@@ -2,9 +2,7 @@
 (function(){
   var React = require('react');
   var ReactDOM = require('react-dom');
-  // Controller View
   var Home = require('./components/appHome.react');
-  // Render
   var App = ReactDOM.render(
     <Home name="Forest" message="Happy to see you on React."/>,
     document.getElementById('react-app')
